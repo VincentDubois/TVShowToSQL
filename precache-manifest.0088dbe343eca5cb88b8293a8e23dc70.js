dbb57f9989ed1d670b00a9df60eaf9f9e3144bd7
Vincent Dubois <dubois@debian>
2019-03-17 21:49:54 +0100

    Updates

---
+++ precache-manifest.0088dbe343eca5cb88b8293a8e23dc70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "788ff4a356cf1bd396ea",
+    "url": "/TVShowToSQL/static/js/runtime~main.a1bccd0c.js"
+  },
+  {
+    "revision": "1748d521e1aead89e16d",
+    "url": "/TVShowToSQL/static/js/main.9be0a501.chunk.js"
+  },
+  {
+    "revision": "44dae87d30b429e590b2",
+    "url": "/TVShowToSQL/static/js/2.2c87ea6b.chunk.js"
+  },
+  {
+    "revision": "1748d521e1aead89e16d",
+    "url": "/TVShowToSQL/static/css/main.f1c0d644.chunk.css"
+  },
+  {
+    "revision": "44dae87d30b429e590b2",
+    "url": "/TVShowToSQL/static/css/2.2f50a11d.chunk.css"
+  },
+  {
+    "revision": "6baf739e9dfb5bc7b5bd763722e12ab9",
+    "url": "/TVShowToSQL/index.html"
+  }
+];
